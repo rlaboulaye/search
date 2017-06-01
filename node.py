c90266e75fdd8d3944a8d098375337108b717332
Roland Laboulaye <rlaboulaye@gmail.com>
2017-06-01 10:23:57 -0600

    parent classes

---
+++ node.py
@@ -0,0 +1,10 @@
+import math
+
+class Node(object):
+
+    def __init__(self, pos):
+        self.pos = pos
+
+    def get_dist(self, node):
+        dist = math.sqrt(((self.pos[0] - node.pos[0]) ** 2) + ((self.pos[1] - node.pos[1]) ** 2))
+
---
+++ search.py
@@ -0,0 +1,11 @@
+
+
+class Search(object):
+
+    def __init__(self, field_dim, tag_radius, robot_radius):
+        self.field_dim = field_dim
+        self.tag_radius = tag_radius
+        self.robot_radius = robot_radius
+
+    def get_path(self, start_pos, end_pos, obstacle_pos, unit_length):
+        pass
