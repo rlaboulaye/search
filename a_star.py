6550129baaab764df355cecac96b2c882b983553
Ryan DeFigueiredo <ryandefig@gmail.com>
2017-06-03 09:41:20 -0600

    A* search working

--- a_star.py
+++ a_star.py
@@ -1,30 +1,38 @@
 import math
-import queue
+import random
+from queue import PriorityQueue
 
 from search import Search
 
 def reconstruct_path(came_from, current_node):
-    path = [current_node]
+    path = [current_node.get_pos()]
     while current_node in came_from.keys():
         current_node = came_from[current_node]
-        path.append(current_node)
-    return path.reverse()
+        path.append(current_node.get_pos())
+    path.reverse()
+    return path
 
 def dist(pos1, pos2):
-    return math.sqrt(((pos2[0] - pos1[0]) ** 2) + ((pos2[1] - pos1[1]) ** 2))
+    return math.sqrt(((pos2.x - pos1.x) ** 2) + ((pos2.y - pos1.y) ** 2))
 
 class AStar(Search):
 
     def __init__(self, field_dim, tag_radius, robot_radius):
         super(AStar, self).__init__(field_dim, tag_radius, robot_radius)
         self.graph = {}
-        self.error_tolerance = 0.05
+        self.error_tolerance = 5
 
     def collision(self, node_pos, obstacle_pos_list, exclude_dist):
         for obstacle_pos in obstacle_pos_list:
-            if dis(node.pos, obstacle_pos) <= exclude_dist:
-                return true
-        return false
+            if dist(node_pos, obstacle_pos) <= exclude_dist:
+                return True
+        return False
+
+    def init_obstacles(self, obstacle_pos_list):
+        pos_obj_list = []
+        for pos in obstacle_pos_list:
+            pos_obj_list.append(Position(pos))
+        return pos_obj_list
 
     def init_graph(self, obstacle_pos_list, unit_length):
         exclude_dist = self.tag_radius + self.robot_radius
@@ -33,68 +41,59 @@ class AStar(Search):
         while y <= self.field_dim[1]:
             x = 0
             while x <= self.field_dim[0]:
-                if not self.collision([x, y], obstacle_pos_list, exclude_dist):
-                    pos = Position([x, y])
-                    graph[pos] = (Node(pos), set())
+                pos = Position([x, y])
+                if not self.collision(pos, obstacle_pos_list, exclude_dist):
+                    self.graph[pos] = (Node(pos), set())
                     left_pos = Position([x - unit_length, y])
                     up_pos = Position([x, y - unit_length])
                     up_left_pos = Position([x - unit_length, y - unit_length])
                     up_right_pos = Position([x + unit_length, y - unit_length])
-                    if left_pos in graph.keys():
-                        graph[pos][1].add(graph[left_pos][0])
-                        graph[left_pos][1].add(graph[pos][0])
-                    if up_pos in graph.keys():
-                        graph[pos][1].add(graph[up_pos][0])
-                        graph[up_pos][1].add(graph[pos][0])
-                    if up_left_pos in graph.keys():
-                        graph[pos][1].add(graph[up_left_pos][0])
-                        graph[up_left_pos][1].add(graph[pos][0])
-                    if up_right_pos in graph.keys():
-                        graph[pos][1].add(graph[up_right_pos][0])
-                        graph[up_right_pos][1].add(graph[pos][0])
+                    if left_pos in self.graph.keys():
+                        self.graph[pos][1].add(self.graph[left_pos][0])
+                        self.graph[left_pos][1].add(self.graph[pos][0])
+                    if up_pos in self.graph.keys():
+                        self.graph[pos][1].add(self.graph[up_pos][0])
+                        self.graph[up_pos][1].add(self.graph[pos][0])
+                    if up_left_pos in self.graph.keys():
+                        self.graph[pos][1].add(self.graph[up_left_pos][0])
+                        self.graph[up_left_pos][1].add(self.graph[pos][0])
+                    if up_right_pos in self.graph.keys():
+                        self.graph[pos][1].add(self.graph[up_right_pos][0])
+                        self.graph[up_right_pos][1].add(self.graph[pos][0])
                 x = x + unit_length
             y = y + unit_length
         return
-#        for node in graph.keys():
-#            for obstacle_pos in obstacle_pos_list:
-#                if dist(node.pos, obstacle_pos) <= exclude_dist:
-#                    del graph[node]
-#        for node1 in graph.keys():
-#            for node2 in graph.keys():
-#                if node1.__eq__(node2):
-#                    continue
-#                if node1 in graph[node2] and node2 in graph[node1]:
-#                    continue
-#                if (node1.pos[0] - node2.pos[0] >= -unit_length and node1.pos[0] - node2.pos[0] <= unit_length and \
-#                    node1.pos[1] - node2.pos[1] >= -unit_length and node1.pos[1] - node2.pos[1] <= unit_length):
-#                    graph[node1].add(node2)
-#                    graph[node2].add(node1)
-#        return graph
-
 
 # start_pos = [x, y], end_pos = [x, y], obstacle_pos = [[x0, y0], [x1, y1], ...], unit_length = d
     def get_path(self, start_pos, end_pos, obstacle_pos, unit_length):
-        self.init_graph(obstacle_pos, unit_length)
+        node_count = 1
+        obstacle_pos_list = self.init_obstacles(obstacle_pos)
+        self.init_graph(obstacle_pos_list, unit_length)
         came_from = {}
-        open_queue = queue.PriorityQueue()
+        open_queue = PriorityQueue()
+        open_set = set()
         closed_set = set()
         start_pos_obj = Position(start_pos)
-        start_node = graph[start_pos_obj][0]
+        start_node = self.graph[start_pos_obj][0]
         start_node.cost_from_start = 0
         start_node.total_cost = start_node.get_heuristic(end_pos)
         open_queue.put((start_node.total_cost, start_node))
+        open_set.add(start_node)
         while not open_queue.empty():
             current_node = open_queue.get()[1]
             if current_node.get_heuristic(end_pos) < self.error_tolerance:
                 return reconstruct_path(came_from, current_node)
+            open_set.remove(current_node)
             closed_set.add(current_node)
-            neighbor_nodes = graph[current_node.pos][1]
+            neighbor_nodes = self.graph[current_node.pos][1]
             for neighbor_node in neighbor_nodes:
                 if neighbor_node in closed_set:
                     continue
                 tentative_cost_from_start = current_node.cost_from_start + dist(current_node.pos, neighbor_node.pos)
-                if neighbor_node not in open_queue:
-                    open_queue.put((tentative_cost_from_start + neighbor_node.get_heuristic, neighbor_node))
+                if neighbor_node not in open_set:
+                    node_count += 1
+                    open_queue.put((tentative_cost_from_start + neighbor_node.get_heuristic(end_pos) + random.random(), neighbor_node))
+                    open_set.add(neighbor_node)
                 elif tentative_cost_from_start >= neighbor_node.cost_from_start:
                     continue
                 came_from[neighbor_node] = current_node
@@ -114,7 +113,11 @@ class Position(object):
         return (self.x == other.x and self.y == other.y)
 
     def __hash__(self):
-        return (x * 53) + (y * 7)
+        return (self.x * 53) + (self.y * 7)
+
+    def get_pos(self):
+        return [self.x, self.y]
+
 
 
 class Node(object):
@@ -125,10 +128,77 @@ class Node(object):
         self.total_cost = float('inf')
 
     def __eq__(self, other):
-        return (self.pos[0] == other.pos[0] and self.pos[1] == other.pos[1])
+        return (self.pos.x == other.pos.x and self.pos.y == other.pos.y)
 
     def __hash__(self):
-        return (pos[0] * 53) + (pos[1] * 7)
+        return (self.pos.x * 53) + (self.pos.y * 7)
 
     def get_heuristic(self, end_pos):
-        return math.sqrt(((end_pos[0] - self.current_pos[0]) ** 2) + ((end_pos[1] - self.current_pos[1]) ** 2))
+        return math.sqrt(((end_pos[0] - self.pos.x) ** 2) + ((end_pos[1] - self.pos.y) ** 2))
+
+    def get_pos(self):
+        return self.pos.get_pos()
+
+
+
+
+
+
+field_dim = [200, 200]
+tag_radius = 5
+robot_radius = 5
+unit_length = 5
+
+start_pos = [25, 25]
+
+obstacle_pos = []
+for x in range(0, 20):
+    new_obstacle = [int(random.randint(2 * tag_radius, field_dim[0] - 2 * tag_radius)), int(random.randint(2 * tag_radius, field_dim[1] - 2 * tag_radius))]
+    obstacle_pos.append(new_obstacle)
+
+end_pos = [int(random.randint(2 * tag_radius, field_dim[0] - 2 * tag_radius)), int(random.randint(2 * tag_radius, field_dim[1] - 2 * tag_radius))]
+
+map_grid = [['-' for x in range(0, int(field_dim[0] / unit_length))] for y in range(0, int(field_dim[1] / unit_length))]
+
+start_y = start_pos[1] - tag_radius
+while start_y <= start_pos[1] + tag_radius:
+    start_x = start_pos[0] - tag_radius
+    while start_x <= start_pos[0] + tag_radius:
+        map_grid[int(start_y / unit_length)][int(start_x / unit_length)] = 'S'
+        start_x += 1
+    start_y += 1
+
+end_y = end_pos[1] - tag_radius
+while end_y <= end_pos[1] + tag_radius:
+    end_x = end_pos[0] - tag_radius
+    while end_x <= end_pos[0] + tag_radius:
+        map_grid[int(end_y / unit_length)][int(end_x / unit_length)] = 'E'
+        end_x += 1
+    end_y += 1
+
+for obstacle in obstacle_pos:
+    obstacle_y = obstacle[1] - tag_radius
+    while obstacle_y <= obstacle[1] + tag_radius:
+        obstacle_x = obstacle[0] - tag_radius
+        while obstacle_x <= obstacle[0] + tag_radius:
+            map_grid[int(obstacle_y / unit_length)][int(obstacle_x / unit_length)] = 'O'
+            obstacle_x += 1
+        obstacle_y += 1
+
+for y in range(0, int(field_dim[1] / unit_length)):
+    for x in range(0, int(field_dim[0] / unit_length)):
+        print(map_grid[y][x], end=' ')
+    print('\n', end='')
+
+a = AStar(field_dim, tag_radius, robot_radius)
+path = a.get_path(start_pos, end_pos, obstacle_pos, unit_length)
+for pos in path:
+    print(pos)
+    map_grid[int(pos[1] / unit_length)][int(pos[0] / unit_length)] = 'X'
+
+for y in range(0, int(field_dim[1] / unit_length)):
+    for x in range(0, int(field_dim[0] / unit_length)):
+        print(map_grid[y][x], end=' ')
+    print('\n', end='')
+
+
